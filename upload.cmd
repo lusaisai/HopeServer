14c6a2d6730d03735bc36739f578a2986610a029
lusaisai <lusaisai@163.com>
2015-10-20 03:18:42 +0800

    refine upload commands

--- upload.cmd
+++ upload.cmd
@@ -1,7 +1,19 @@
+:: The first one may ask for login info
 appcfg.py --application=gae0x0000 update .
-appcfg.py --application=gae0x0001 update .
-appcfg.py --application=gae0x0002 update .
-appcfg.py --application=gae0x0003 update .
-appcfg.py --application=gae0x0004 update .
-appcfg.py --application=gae0x0005 update .
-appcfg.py --application=gae0x0006 update .
+
+:: The following will run in parallel
+start "upload" appcfg.py --application=gae0x0001 update .
+start "upload" appcfg.py --application=gae0x0002 update .
+start "upload" appcfg.py --application=gae0x0003 update .
+start "upload" appcfg.py --application=gae0x0004 update .
+start "upload" appcfg.py --application=gae0x0005 update .
+start "upload" appcfg.py --application=gae0x0006 update .
+start "upload" appcfg.py --application=gae0x0007 update .
+start "upload" appcfg.py --application=gae0x0008 update .
+start "upload" appcfg.py --application=gae0x0009 update .
+start "upload" appcfg.py --application=gae0x000a update .
+start "upload" appcfg.py --application=gae0x000b update .
+start "upload" appcfg.py --application=gae0x000c update .
+start "upload" appcfg.py --application=gae0x000d update .
+start "upload" appcfg.py --application=gae0x000e update .
+start "upload" appcfg.py --application=gae0x000f update .
